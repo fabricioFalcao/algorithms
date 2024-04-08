95cae3146300e03f4b57dec83cac53accc85ffe1
fabricioFalcao <fabriciohfd@gmail.com>
2024-04-08 08:46:53 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    try:
+        assert 0 <= target_time <= 24
+        for log_in_time, log_out_time in permanence_period:
+            assert 0 <= log_in_time <= 24
+            assert 0 <= log_out_time <= 24
+
+            if  log_in_time <= target_time <= log_out_time:
+                counter += 1
+    except (TypeError, AssertionError, ValueError):
+        return None
+    
+    return counter
