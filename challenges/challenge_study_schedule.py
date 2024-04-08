abdd5c4a5422f34a393d736b2229154fbada7e30
fabricioFalcao <fabriciohfd@gmail.com>
2024-04-08 08:55:43 -0300

    Requisito 1 - lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -6,9 +6,9 @@ def study_schedule(permanence_period, target_time):
             assert 0 <= log_in_time <= 24
             assert 0 <= log_out_time <= 24
 
-            if  log_in_time <= target_time <= log_out_time:
+            if log_in_time <= target_time <= log_out_time:
                 counter += 1
     except (TypeError, AssertionError, ValueError):
         return None
-    
+
     return counter
