fde58ab496ef3f95a6e3e277f9f1de27563877d3
fabricioFalcao <fabriciohfd@gmail.com>
2024-04-08 15:03:36 -0300

    Requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,31 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not (isinstance(nums, list) and len(nums) > 1):
+        return False
+
+    duplicate = set()
+
+    for number in nums:
+        if not (isinstance(number, int) and number >= 1):
+            return False
+        elif number in duplicate:
+            return number
+        else:
+            duplicate.add(number)
+    return False
+
+
+# def find_duplicate(nums):
+#     if not (isinstance(nums, list) and len(nums) > 1):
+#         return False
+
+#     try:
+#         nums.sort()
+
+#         for i in range(1, len(nums)):
+#             if nums[i] >= 1 and nums[i] == nums[i - 1]:
+#                 return nums[i]
+
+#         return False
+
+#     except Exception:
+#         return False
